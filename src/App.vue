4c93bb6df214ae2d1f44c198df6c9ff729ee5764
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2022-07-28 10:02:23 +0300

    ui, history of old urls and hashes, copy

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
-import { computed, reactive, ref } from "vue"
+import { computed, reactive, Ref, ref } from "vue"
 
-const data = reactive({
+interface IData {
+  givenUrl: String
+  hash: String
+  tinyrl: String
+}
+
+const data: IData = reactive({
   givenUrl: "",
   hash: "",
   tinyrl: "",
 })
-const loading = ref(false)
+const loading: Ref<boolean> = ref(false)
 
-const saveHash = (url) => {
+const saveHash = (url: String) => {
   loading.value = true
   setTimeout(() => {
-    const generatedShortUrl = generateHash()
+    const generatedShortUrl = generateHash.value
     loading.value = false
     data.hash = generatedShortUrl
     data.tinyrl = `${window.location.origin}/${generatedShortUrl}`
@@ -23,12 +29,33 @@ const saveHash = (url) => {
   }, 2000)
 }
 
-const generateHash = () => (Math.random() + 1).toString(36).substring(7)
-
-const historicalUrls = computed(() => {
+const generateHash = computed<String>(() =>
+  (Math.random() + 1).toString(36).substring(7)
+)
+const historicalUrls = computed<any>(() => {
   const urls = localStorage.getItem("historicalUrls")
-  return urls ? urls : []
+  return urls ? JSON.parse(urls) : []
 })
+
+const copyToClipboard = (url: string) => {
+  const el = document.createElement("textarea")
+  el.value = url
+  document.body.appendChild(el)
+  el.select()
+  document.execCommand("copy")
+  document.body.removeChild(el)
+}
+const copy = (historicalUrl: IData) => {
+  copyToClipboard(historicalUrl.tinyrl.toString())
+  const btn = copyBtns.value[historicalUrl.hash.toString()]
+  btn.classList.add("success")
+  btn.innerText = "Copied!"
+  setTimeout(() => {
+    btn.classList.remove("success")
+    btn.innerText = "Copy"
+  }, 2000)
+}
+const copyBtns = ref<any>({})
 </script>
 
 <template>
@@ -36,7 +63,7 @@ const historicalUrls = computed(() => {
     <div class="container">
       {{ loading ? "Loading..." : "" }}
       <div class="row flex justify-content-center">
-        <div class="col-lg-8">
+        <div class="col-lg-9 col-12">
           <input
             v-model="data.givenUrl"
             type="text"
@@ -44,20 +71,50 @@ const historicalUrls = computed(() => {
             placeholder="Enter a URL to tinify"
           />
         </div>
-        <div class="mr-3"></div>
-        <div class="col-lg-2">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-2 col-12 mt-2">
           <button
             @click="() => saveHash(data.givenUrl)"
-            class="block blue create-button"
+            class="block blue create-button tinify-button"
           >
             Tinify
           </button>
         </div>
       </div>
       <div class="row flex justify-content-center">
-        <div class="card">
-          <div class="card-body">
-            <!-- get from local storage -->
+        <div class="card col-12 mt-3 border-radius-1 p-2">
+          <!-- get from local storage -->
+          <div
+            class="row flex justify-content-center align-items-center mt-2 p-2"
+            v-for="historicalUrl in historicalUrls"
+            :key="historicalUrl.hash"
+          >
+            <div class="col-lg-6 col-md-12 flex justify-content-start">
+              <p>{{ historicalUrl.givenUrl }}</p>
+            </div>
+            <div class="col-lg-6 col-md-12">
+              <div class="row flex align-items-center">
+                <div class="col-lg-9 col-md-12 col-12">
+                  <p class="text-right mr-4">
+                    <a
+                      class="link"
+                      :href="historicalUrl.tinyrl"
+                      target="_blank"
+                      >{{ historicalUrl.tinyrl }}</a
+                    >
+                  </p>
+                </div>
+                <div class="col-lg-3 col-md-12 col-12">
+                  <button
+                    v-on:click="() => copy(historicalUrl)"
+                    class="light text-dark block create-button copy-btn"
+                    :ref="(el) => (copyBtns[historicalUrl.hash] = el)"
+                  >
+                    Copy
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -70,8 +127,25 @@ input {
   height: 60px;
   border-radius: 1rem;
 }
-button {
+.tinify-button {
   height: 60px;
   border-radius: 1rem;
+}
+
+.copy-btn {
+  border-radius: 0.6rem;
+  border: none;
+  font-size: 1rem;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    background-color: #e9e9e9;
+  }
+}
+
+.success {
+  background-color: #4caf50 !important;
+  color: #fff !important;
 }
 </style>
