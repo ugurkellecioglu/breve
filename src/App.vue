a0a5de599cd1d39ad5568c4042eaa74b12629704
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2022-07-28 07:38:34 +0300

    shorten url UI and create function

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue"
+
+const url = ref("")
+const loading = ref(false)
+const shortenUrl = (url) => {
+  loading.value = true
+  setTimeout(() => {
+    const generatedShortUrl = generateShortUrl()
+    loading.value = false
+    return generatedShortUrl
+  }, 2000)
+}
+
+const generateShortUrl = () => (Math.random() + 1).toString(36).substring(7)
+</script>
 
 <template>
   <div class="wrapper">
-    <input type="text" class="create" placeholder="Create a new link" />
-    <button class="create-button">Create</button>
+    <div class="container">
+      {{ loading ? "Loading..." : "" }}
+      <div class="row flex justify-content-center">
+        <div class="col-lg-8">
+          <input
+            v-model="url"
+            type="text"
+            class="create block"
+            placeholder="Enter a URL to shorten"
+          />
+        </div>
+        <div class="col-lg-4">
+          <button
+            @click="() => shortenUrl(url)"
+            class="block green create-button"
+          >
+            Create
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
